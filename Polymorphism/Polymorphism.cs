d287d8a86ad0a9fc7c09c719aafab35b07d4e199
nigamabhay671 <nigamabhay671@users.noreply.github.com>
2020-02-22 01:33:16 +0530

    Add project files.

---
+++ Polymorphism/Polymorphism.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    class Polymorphism
+    {
+        public void Add()
+        {
+            int a = 10;
+            int b = 3;
+            int c = a + b;
+            Console.WriteLine(c) ;
+        }
+        public void Add(int a,int b)
+        {           
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+        public void Add(int a,int b,int d)
+        {
+            int c = a + b+d;
+            Console.WriteLine(c);
+        }
+
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polymorphism p = new Polymorphism();
+            p.Add();
+            p.Add(2, 3);
+            p.Add(1, 2, 3);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
